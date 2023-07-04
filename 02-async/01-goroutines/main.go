69a8c76c92870237905518f3bf42e6a6a8eb313d
Vitaly Dudov <vdudov@iponweb.net>
2023-07-04 20:51:41 +0300

    first done, second recived

---
+++ 02-async/01-goroutines/main.go
@@ -0,0 +1,51 @@
+package main
+
+type User struct {
+	Email string
+}
+
+type UserRepository interface {
+	CreateUserAccount(u User) error
+}
+
+type NotificationsClient interface {
+	SendNotification(u User) error
+}
+
+type NewsletterClient interface {
+	AddToNewsletter(u User) error
+}
+
+type Handler struct {
+	repository          UserRepository
+	newsletterClient    NewsletterClient
+	notificationsClient NotificationsClient
+}
+
+func NewHandler(
+	repository UserRepository,
+	newsletterClient NewsletterClient,
+	notificationsClient NotificationsClient,
+) Handler {
+	return Handler{
+		repository:          repository,
+		newsletterClient:    newsletterClient,
+		notificationsClient: notificationsClient,
+	}
+}
+
+func (h Handler) SignUp(u User) error {
+	if err := h.repository.CreateUserAccount(u); err != nil {
+		return err
+	}
+
+	if err := h.newsletterClient.AddToNewsletter(u); err != nil {
+		return err
+	}
+
+	if err := h.notificationsClient.SendNotification(u); err != nil {
+		return err
+	}
+
+	return nil
+}
